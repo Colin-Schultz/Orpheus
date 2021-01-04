78c2f6856f77faa78d5e54b3512f3f441e157341
Colin-Schultz-Programming <64875328+Colin-Schultz-Programming@users.noreply.github.com>
2021-01-04 12:59:55 -0500

    Env Variables

--- src/main/java/OrpheusDriver.java
+++ src/main/java/OrpheusDriver.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class OrpheusDriver {
 
     public static void main(String[] args) throws Exception{
-        JDA jda = JDABuilder.createDefault("Nzk1NjgwNDk1MTk0ODY1NzM2.X_M5NQ.hh_j_AxGbh6PjLFiY_ijyk6vprQ").build();
+        JDA jda = JDABuilder.createDefault(System.getenv("BOT_TOKEN")).build();
 
         jda.addEventListener(new TextEventListener());
         jda.addEventListener(new AudioEventListener());
